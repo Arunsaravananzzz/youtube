b1b4c4ff8951f70e2d820d1951a0c7f13b584349
nila <nilavazhagan97@gmail.com>
2020-07-02 16:34:40 +0530

    try

---
+++ first.js
@@ -0,0 +1,21 @@
+let a=[1,2,30,5,40];
+let b=[10,20,30,40,50];
+let c=[1,30,2,40]
+let i=0,j=0,k=0;
+for(i=0;i<a.length;i++)
+{
+    for(j=0;j<b.length;j++){
+    if(a[i]==b[j])
+    {
+        for(k=0;k<c.length;k++)
+        {
+            if(a[i]==c[k])
+            {
+                console.log(a[i])
+            }
+            else{}
+        }
+    }
+    else{}
+}
+}
